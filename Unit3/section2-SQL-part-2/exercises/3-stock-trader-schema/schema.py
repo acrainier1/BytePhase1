22d55cab61570b0c2610b311753cf29761e825cc
acrainier1 <alex.c.canizales@gmail.com>
2019-10-31 16:44:33 -0400

    account.py

--- Unit3/section2-SQL-part-2/exercises/3-stock-trader-schema/schema.py
+++ Unit3/section2-SQL-part-2/exercises/3-stock-trader-schema/schema.py
@@ -1,37 +1,40 @@
 import sqlite3 
 
 
-CREATE_SQL = """
+CREATE_SQL_ACCOUNT = """
 CREATE TABLE account(
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    username VARCHAR(32),
+    username VARCHAR(32) NOT NULL,
     password_hash VARCHAR(255),
     balance FLOAT,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    email_address VARCHAR(128));
+    email_address VARCHAR(128));"""
 
+
+CREATE_SQL_POSITION = """
 CREATE TABLE position(
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    ticker VARCHAR(16),
-    shares FLOAT),
+    ticker VARCHAR(16) NOT NULL,
+    shares FLOAT,
     account_id INTEGER,
-    FOREIGN KEY (`account_id) REFERENCES account(id));
+    FOREIGN KEY (`account_id) REFERENCES account(id));"""
 
+
+CREATE_SQL_TRADE = """
 CREATE TABLE trade(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    position_id INTEGER,
-    ticker VARCHAT(16),
+    ticker VARCHAT(16) NOT NULL,
     volume INTEGER,
     unit_price FLOAT,
     time DATE,
     account_id INTEGER,
-    FOREIGN KEY (`position_id`) REFERENCES position(id),
-    FOREIGN KEY (`ticker`) REFERENCES position(ticker), ???????????????
-    FOREIGN KEY (`account_id) REFERENCES account(id));
+    FOREIGN KEY (`account_id) REFERENCES account(id));"""
 """
 
-DROP_SQL = "DROP TABLE IF EXISTS stocktrader"
+DROP_SQL_ACCOUNT = "DROP TABLE IF EXISTS account"
+DROP_SQL_POSITION = "DROP TABLE IF EXISTS position"
+DROP_SQL_TRADE = "DROP TABLE IF EXISTS trade"
 
 with sqlite3.connect("stocktrader.db") as conn:
     cursor = conn.cursor() # cursor executes statements, connect connects the statements
