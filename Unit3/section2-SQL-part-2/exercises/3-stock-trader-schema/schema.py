07e65da3074f6d052b5608c9de7d5771a4ab2c74
acrainier1 <alex.c.canizales@gmail.com>
2019-11-04 16:09:21 -0500

    notes

--- Unit3/section2-SQL-part-2/exercises/3-stock-trader-schema/schema.py
+++ Unit3/section2-SQL-part-2/exercises/3-stock-trader-schema/schema.py
@@ -18,7 +18,7 @@ CREATE TABLE position(
     ticker VARCHAR(16) NOT NULL,
     shares FLOAT,
     account_id INTEGER,
-    FOREIGN KEY (`account_id) REFERENCES account(id));"""
+    FOREIGN KEY (`account_id`) REFERENCES account(id));"""
 
 
 CREATE_SQL_TRADE = """
@@ -29,14 +29,14 @@ CREATE TABLE trade(
     unit_price FLOAT,
     time DATE,
     account_id INTEGER,
-    FOREIGN KEY (`account_id) REFERENCES account(id));"""
-"""
+    FOREIGN KEY (`account_id`) REFERENCES account(id));"""
 
-DROP_SQL_ACCOUNT = "DROP TABLE IF EXISTS account"
-DROP_SQL_POSITION = "DROP TABLE IF EXISTS position"
-DROP_SQL_TRADE = "DROP TABLE IF EXISTS trade"
+DROP_SQL = {"DROP_SQL_ACCOUNT": "DROP TABLE IF EXISTS account",
+            "DROP_SQL_POSITION": "DROP TABLE IF EXISTS position",
+            "DROP_SQL_TRADE": "DROP TABLE IF EXISTS trade"}
 
 with sqlite3.connect("stocktrader.db") as conn:
-    cursor = conn.cursor() # cursor executes statements, connect connects the statements
-    cursor.execute(DROP_SQL) # deletes table: accounts if already exists
+    cursor = conn.cursor() # cursor executes statements, connect connects the statements  
+    for k,v in DROP_SQL.items():
+        cursor.execute(k) # deletes table: accounts if already exists
     cursor.execute(CREATE_SQL)
